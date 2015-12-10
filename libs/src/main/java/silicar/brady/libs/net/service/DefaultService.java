b75c595c0fc94f7c9c6e384d982501fc3fd725de
tutu <tutu.liu@cloudtopcn.com>
2015-12-10 17:37:33 +0800

    更改SDk版本,添加网络接口解析模板

---
+++ libs/src/main/java/silicar/brady/libs/net/service/DefaultService.java
@@ -0,0 +1,11 @@
+package silicar.brady.libs.net.service;
+
+/**
+ * 网络请求解析模板
+ * Created by Work on 2015/12/10.
+ * @version 1.0
+ * @since 2015/12/10
+ * @author tutu
+ */
+public class DefaultService {
+}
